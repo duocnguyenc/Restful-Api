e45322116441027337986c16dc251a02c11ba06c
Duoc <satthukt1232@gmail.com>
2017-10-05 11:00:21 +0700

    Week 1

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,9 +65,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-             ->middleware('api')
+        // duoc.restful.com/api/...
+        Route::middleware('api')
              ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+             ->group(base_path('routes/api.php'));   
     }
 }
