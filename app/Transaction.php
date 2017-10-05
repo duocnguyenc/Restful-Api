90517b518b9fd4d32e22edb951e367a125ccd1c6
Duoc <satthukt1232@gmail.com>
2017-10-05 15:04:07 +0700

    Implementing The Relationships Between Models

--- app/Transaction.php
+++ app/Transaction.php
@@ -3,12 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Buyer;
+use App\Product;
 
 class Transaction extends Model
 {
     protected $fillable = [
-    	'quantity',
-    	'buyer_id',
-    	'product_id',
+        'quantity',
+        'buyer_id',
+        'product_id',
     ];
+
+    public function buyer()
+    {
+        return $this->belongsTo(Buyer::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
