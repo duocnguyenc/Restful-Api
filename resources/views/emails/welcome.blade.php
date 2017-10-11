473497da6f96f6fdf9520be32c5ef781747c38d1
Duoc <satthukt1232@gmail.com>
2017-10-11 13:47:16 +0700

    Implementing the UserCreated Mailable

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,3 +1,3 @@
 Hello {{$user->name}}
 Thank you for create an account. Please vetify your email using link:
-{{route('vetify', $user->verification_token)}}
+{{route('verify', $user->verification_token)}}
