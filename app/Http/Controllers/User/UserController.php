48df10dc719c275b6fcb39333db328717e136625
Duoc <satthukt1232@gmail.com>
2017-10-11 11:12:01 +0700

    Implementing the Verification System

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -111,4 +111,16 @@ class UserController extends ApiController
         $user->delete();
         return $this->showOne($user);
     }
+
+    public function verify($token)
+    {
+        $user = User::where('verification_token', $token)->firstOrFail();
+
+        $user->verified = User::VERIFIED_USER;
+        $user->verification_token = null;
+
+        $user->save();
+
+        return $this->showMessage('The account has been verified succesfully');
+    }
 }
