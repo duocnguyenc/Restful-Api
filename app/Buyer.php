235d19edde04c47f9a3d30ffc53d9c209250318a
Duoc <satthukt1232@gmail.com>
2017-10-12 17:20:14 +0700

    [138] Linking the Models with its Respective Transformer

--- app/Buyer.php
+++ app/Buyer.php
@@ -4,9 +4,12 @@ namespace App;
 
 use App\Scopes\BuyerScope;
 use App\Transaction;
+use App\Transformers\BuyerTransformer;
 
 class Buyer extends User
 {
+	public $transformer = BuyerTransformer::class;
+
     protected static function boot()
     {
         parent::boot();
