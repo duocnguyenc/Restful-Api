e91a12f0b85f63fe85363af0ed437596df1e172c
Duoc Nguyen C <satthukt1232@gmail.com>
2017-10-18 15:31:05 +0700

    [172] Making Users Authenticatable Through API Tokens

--- app/User.php
+++ app/User.php
@@ -6,10 +6,11 @@ use App\Transformers\UserTransformer;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     const VERIFIED_USER = '1';
     const UNVERIFIED_USER = '0';
