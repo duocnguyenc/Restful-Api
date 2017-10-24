fa7c06016058e715e5af0505e276e634b07935b6
Duoc Nguyen C <satthukt1232@gmail.com>
2017-10-24 14:39:25 +0700

    [183] Allowing the Management of API Clients

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,8 +16,16 @@ window.Vue = require('vue');
  */
 
 Vue.component(
-	'passport-personal-access-tokens',
-	require('./components/passport/PersonalAccessTokens.vue'));
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue'));
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue'));
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue'));
 
 const app = new Vue({
     el: '#app'
