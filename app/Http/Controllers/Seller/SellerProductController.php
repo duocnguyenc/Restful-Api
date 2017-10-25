229a20d02fad86f8f4cc4c6dbadbc0e6cb248f22
Duoc Nguyen C <satthukt1232@gmail.com>
2017-10-25 15:11:58 +0700

    [193] Restricting the Actions that Need the “read-general” Scope

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -7,6 +7,7 @@ use App\Product;
 use App\Seller;
 use App\Transformers\ProductTransformer;
 use App\User;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -18,7 +19,7 @@ class SellerProductController extends ApiController
         parent::__construct();
 
         $this->middleware('transform.input:' . ProductTransformer::class)->only(['store', 'update']);
-        $this->middleware('scope:manage-products');
+        $this->middleware('scope:manage-products')->except('index');
     }
 
     /**
@@ -28,9 +29,12 @@ class SellerProductController extends ApiController
      */
     public function index(Seller $seller)
     {
-        $products = $seller->products;
-
-        return $this->showAll($products);
+        if (request()->user()->tokenCan('read-general') || request()->user()->tokenCan('manage-products') ) {
+            $products = $seller->products;
+            return $this->showAll($products);
+        }
+        throw new AuthorizationException('Invalid scope(s)');
+        
     }
 
     /**
